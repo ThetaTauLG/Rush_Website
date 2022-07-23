9e6b92cb4aadbecfb6cc6905a38fe80e8bcfe1cc
Chase Guynup <chasepg0821@gmail.com>
2022-07-23 10:39:39 -0400

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export default function index() {
-    return ( <
-        div >
-        hello <
-        /div>
+    return (
+        <div>
+            hello
+        </div>
     )
 }
