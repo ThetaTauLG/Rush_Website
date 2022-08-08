479abcf39fe67819154c1d4106b92887e1644b9d
Chase Guynup <chasepg0821@gmail.com>
2022-08-08 19:34:30 -0400

    changes

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,13 +1,7 @@
-import { Grid, Paper, Container, Typography } from '@mui/material'
+import { Grid, Paper, Container, Typography, Stack, IconButton } from '@mui/material'
 import { StaticImage } from 'gatsby-plugin-image'
-import { styled } from '@mui/system'
 import React from 'react'
-
-const HeroContainer = styled(Container)({
-    position: 'relative',
-    zIndex: 100,
-    height: '100%',
-});
+import { Instagram } from '@mui/icons-material'
 
 export default function HeroSection() {
     return (
@@ -18,13 +12,30 @@ export default function HeroSection() {
             <StaticImage src='../images/banner3.jpeg' style={{ height: '90vh', position: 'absolute' }} />
             <div style={{ backgroundColor: 'rgba(0,0,0,.3)', width: '100%', height: '100%', position: 'absolute' }}></div>
 
-            <HeroContainer maxWidth='lg'>
+            <Container maxWidth='lg' sx={{
+                position: 'relative',
+                zIndex: 100,
+                height: '100%',
+            }}>
                 <Grid container justifyContent='space-between' alignItems='center' sx={{ height: '100%' }}>
                     <Grid item>
-                        <Typography variant='h3' component='h1'>Theta Tau Rush</Typography>
+                        <Typography variant='h3' component='h1' sx={{ color: 'white' }}>Theta Tau Rush</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Stack direction='column'>
+                            <IconButton color='white' size='large'>
+                                <Instagram />
+                            </IconButton>
+                            <IconButton color='white' size='large'>
+                                <Instagram />
+                            </IconButton>
+                            <IconButton color='white' size='large'>
+                                <Instagram />
+                            </IconButton>
+                        </Stack>
                     </Grid>
                 </Grid>
-            </HeroContainer>
+            </Container>
 
         </Paper>
     )
