69f4baaecbb6208153355ece9bb7a40ff51b1166
Chase Guynup <chasepg0821@gmail.com>
2022-08-08 16:42:55 -0400

    banner and zindex fix

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,16 +1,31 @@
-import { Paper } from '@mui/material'
-import Banner from '../images/banner.png'
+import { Grid, Paper, Container, Typography } from '@mui/material'
+import { StaticImage } from 'gatsby-plugin-image'
+import { styled } from '@mui/system'
 import React from 'react'
 
+const HeroContainer = styled(Container)({
+    position: 'relative',
+    zIndex: 100,
+    height: '100%',
+});
 
 export default function HeroSection() {
     return (
         <Paper sx={{
             height: '90vh',
-            backgroundImage: `url(${Banner})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center'
         }}>
+
+            <StaticImage src='../images/banner3.jpeg' style={{ height: '90vh', position: 'absolute' }} />
+            <div style={{ backgroundColor: 'rgba(0,0,0,.3)', width: '100%', height: '100%', position: 'absolute' }}></div>
+
+            <HeroContainer maxWidth='lg'>
+                <Grid container justifyContent='space-between' alignItems='center' sx={{ height: '100%' }}>
+                    <Grid item>
+                        <Typography variant='h3' component='h1'>Theta Tau Rush</Typography>
+                    </Grid>
+                </Grid>
+            </HeroContainer>
+
         </Paper>
     )
 }
