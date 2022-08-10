036c83a66d91fa87ee9507babf45073970d5cb76
Chase Guynup <chasepg0821@gmail.com>
2022-08-10 01:00:57 -0400

    meta changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,18 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { Helmet } from 'react-helmet';
 
 
-export default function PersistentDrawerLeft() {
+export default function Index() {
     return (
-        <App />
+        <div>
+            <Helmet>
+                <title>Theta Tau Rush</title>
+                <meta name='description' content="Clemson University's chapter of Theta Tau, Lamba Gamma's, rush info, schedule and contacts." />
+            </Helmet>
+            <App />
+        </div>
+
     );
 }
