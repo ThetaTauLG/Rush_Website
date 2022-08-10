be89727dd720bb3d9ce98fc9e97927a1597af8b3
Chase Guynup <chasepg0821@gmail.com>
2022-08-09 23:58:52 -0400

    image compression and schedule finalize

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,11 +1,11 @@
 import { Box, Card, Container, Grid, Paper, Stack, Typography } from '@mui/material'
 import React from 'react'
-import InfoNights from '../images/info_nights.jpeg'
-import Cookout from '../images/cookout.jpeg'
-import Sports from '../images/sports.jpeg'
-import CommServ from '../images/comm_serve.jpeg'
-import Interview from '../images/interview.jpeg'
-import Dinner from '../images/family_dinner.jpeg'
+import InfoNights from '../images/info_nights.webp'
+import Cookout from '../images/cookout.webp'
+import Sports from '../images/sports.webp'
+import CommServ from '../images/comm_serve.webp'
+import Interview from '../images/interview.webp'
+import Dinner from '../images/family_dinner.webp'
 
 export default function Schedule() {
     return (
@@ -23,6 +23,27 @@ export default function Schedule() {
                             backgroundPosition: 'top',
                             height: '20vh',
                         }}>
+                            <Grid container justifyContent='left' alignItems='center' sx={{
+                                height: '100%',
+                            }}>
+                                <Grid item xs={5} sx={{
+                                    height: '100%',
+                                }}>
+                                    <Card sx={{
+                                        height: '100%',
+                                        width: '100%',
+                                        backgroundColor: 'rgba(139,0,0,0.55)',
+                                    }}>
+                                        <Box m={3}>
+                                            <Stack alignContent='center' direction='column'>
+                                                <Typography align='left' variant='h3' sx={{ color: 'primary.contrastText' }}>Dates</Typography>
+                                                <Typography align='left' variant='h4' sx={{ color: 'primary.contrastText' }}>Event</Typography>
+                                                <Typography align='left' variant='subtitle1' sx={{ color: 'primary.contrastText' }}>snippet of event</Typography>
+                                            </Stack>
+                                        </Box>
+                                    </Card>
+                                </Grid>
+                            </Grid>
                         </Paper>
                     </Grid>
                     <Grid item xs={12}>
@@ -32,6 +53,27 @@ export default function Schedule() {
                             backgroundPosition: 'center',
                             height: '20vh',
                         }}>
+                            <Grid container justifyContent='right' alignItems='center' sx={{
+                                height: '100%',
+                            }}>
+                                <Grid item xs={5} sx={{
+                                    height: '100%',
+                                }}>
+                                    <Card sx={{
+                                        height: '100%',
+                                        width: '100%',
+                                        backgroundColor: 'rgba(139,0,0,0.55)',
+                                    }}>
+                                        <Box m={3}>
+                                            <Stack alignContent='center' direction='column'>
+                                                <Typography align='right' variant='h3' sx={{ color: 'primary.contrastText' }}>Dates</Typography>
+                                                <Typography align='right' variant='h4' sx={{ color: 'primary.contrastText' }}>Event</Typography>
+                                                <Typography align='right' variant='subtitle1' sx={{ color: 'primary.contrastText' }}>snippet of event</Typography>
+                                            </Stack>
+                                        </Box>
+                                    </Card>
+                                </Grid>
+                            </Grid>
                         </Paper>
                     </Grid>
                     <Grid item xs={12}>
@@ -41,6 +83,27 @@ export default function Schedule() {
                             backgroundPosition: 'bottom',
                             height: '20vh',
                         }}>
+                            <Grid container justifyContent='left' alignItems='center' sx={{
+                                height: '100%',
+                            }}>
+                                <Grid item xs={5} sx={{
+                                    height: '100%',
+                                }}>
+                                    <Card sx={{
+                                        height: '100%',
+                                        width: '100%',
+                                        backgroundColor: 'rgba(139,0,0,0.55)',
+                                    }}>
+                                        <Box m={3}>
+                                            <Stack alignContent='center' direction='column'>
+                                                <Typography align='left' variant='h3' sx={{ color: 'primary.contrastText' }}>Dates</Typography>
+                                                <Typography align='left' variant='h4' sx={{ color: 'primary.contrastText' }}>Event</Typography>
+                                                <Typography align='left' variant='subtitle1' sx={{ color: 'primary.contrastText' }}>snippet of event</Typography>
+                                            </Stack>
+                                        </Box>
+                                    </Card>
+                                </Grid>
+                            </Grid>
                         </Paper>
                     </Grid>
                     <Grid item xs={12}>
@@ -50,6 +113,27 @@ export default function Schedule() {
                             backgroundPosition: 'bottom',
                             height: '20vh',
                         }}>
+                            <Grid container justifyContent='right' alignItems='center' sx={{
+                                height: '100%',
+                            }}>
+                                <Grid item xs={5} sx={{
+                                    height: '100%',
+                                }}>
+                                    <Card sx={{
+                                        height: '100%',
+                                        width: '100%',
+                                        backgroundColor: 'rgba(139,0,0,0.55)',
+                                    }}>
+                                        <Box m={3}>
+                                            <Stack alignContent='center' direction='column'>
+                                                <Typography align='right' variant='h3' sx={{ color: 'primary.contrastText' }}>Dates</Typography>
+                                                <Typography align='right' variant='h4' sx={{ color: 'primary.contrastText' }}>Event</Typography>
+                                                <Typography align='right' variant='subtitle1' sx={{ color: 'primary.contrastText' }}>snippet of event</Typography>
+                                            </Stack>
+                                        </Box>
+                                    </Card>
+                                </Grid>
+                            </Grid>
                         </Paper>
                     </Grid>
                     <Grid item xs={12}>
@@ -59,6 +143,27 @@ export default function Schedule() {
                             backgroundPosition: 'center',
                             height: '20vh',
                         }}>
+                            <Grid container justifyContent='left' alignItems='center' sx={{
+                                height: '100%',
+                            }}>
+                                <Grid item xs={5} sx={{
+                                    height: '100%',
+                                }}>
+                                    <Card sx={{
+                                        height: '100%',
+                                        width: '100%',
+                                        backgroundColor: 'rgba(139,0,0,0.55)',
+                                    }}>
+                                        <Box m={3}>
+                                            <Stack alignContent='center' direction='column'>
+                                                <Typography align='left' variant='h3' sx={{ color: 'primary.contrastText' }}>Dates</Typography>
+                                                <Typography align='left' variant='h4' sx={{ color: 'primary.contrastText' }}>Event</Typography>
+                                                <Typography align='left' variant='subtitle1' sx={{ color: 'primary.contrastText' }}>snippet of event</Typography>
+                                            </Stack>
+                                        </Box>
+                                    </Card>
+                                </Grid>
+                            </Grid>
                         </Paper>
                     </Grid>
                     <Grid item xs={12}>
@@ -68,6 +173,27 @@ export default function Schedule() {
                             backgroundPosition: 'center',
                             height: '20vh',
                         }}>
+                            <Grid container justifyContent='right' alignItems='center' sx={{
+                                height: '100%',
+                            }}>
+                                <Grid item xs={5} sx={{
+                                    height: '100%',
+                                }}>
+                                    <Card sx={{
+                                        height: '100%',
+                                        width: '100%',
+                                        backgroundColor: 'rgba(139,0,0,0.55)',
+                                    }}>
+                                        <Box m={3}>
+                                            <Stack alignContent='center' direction='column'>
+                                                <Typography align='right' variant='h3' sx={{ color: 'primary.contrastText' }}>Dates</Typography>
+                                                <Typography align='right' variant='h4' sx={{ color: 'primary.contrastText' }}>Event</Typography>
+                                                <Typography align='right' variant='subtitle1' sx={{ color: 'primary.contrastText' }}>snippet of event</Typography>
+                                            </Stack>
+                                        </Box>
+                                    </Card>
+                                </Grid>
+                            </Grid>
                         </Paper>
                     </Grid>
                 </Grid>
