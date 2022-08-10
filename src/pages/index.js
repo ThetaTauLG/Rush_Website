94fe3dd93d4050f9eaa4d15183ce43836dc1c468
Chase Guynup <chasepg0821@gmail.com>
2022-08-10 01:10:26 -0400

    meta changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,8 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { Helmet } from 'react-helmet';
+import Helmet from 'react-helmet';
+import favicon from '../images/crest.png'
 
 
 export default function Index() {
@@ -13,6 +14,7 @@ export default function Index() {
             <Helmet>
                 <title>Theta Tau Rush</title>
                 <meta name='description' content="Clemson University's chapter of Theta Tau, Lamba Gamma's, rush info, schedule and contacts." />
+                <link rel='icon' href={favicon} type='image/png' sizes='16x16' />
             </Helmet>
             <App />
         </div>
